cf133ca2f0b21e3ec3f00eb41d7c73160bd2a257
khalid <khalidcomilla58@gmail.com>
2024-11-16 22:07:50 +0600

    navigation bar modified

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -68,14 +68,14 @@ function Navbar() {
     <div className="navbar bg-base-100">
         <div className="navbar-start">
             <div className="dropdown">
-                <div tabIndex={0} role="button" className="lg:hidden mr-4">
+                <div tabIndex={0} role="button" className="mr-4">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </div>
                 <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                 {list}
                 </ul>
             </div>
-            <Link className="font-bold text-xl mx-[-4px]" to = '/'>
+            <Link className="font-bold text-xl mx-[-4px] flex justify-end w-full" to = '/'>
                 <p className="flex items-center gap-2 text-lg font-semibold text-gray-700 hover:text-blue-600 transition duration-300">
                     <img
                         src="/truck-solid.svg"
@@ -89,11 +89,11 @@ function Navbar() {
             </Link>
         </div>
 
-        <div className="navbar-center hidden lg:flex">
+        {/* <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal ">
                 {list}
             </ul>
-        </div>
+        </div> */}
 
         <div className="navbar-end">
             {
